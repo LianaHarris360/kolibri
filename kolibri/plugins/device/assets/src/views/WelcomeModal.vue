875d028d11eb2b0bc93b6e8b16ddd376a0b222a2
shruti <shrutiyadav862@gmail.com>
2025-08-27 16:03:28 +0530

    Made HomePageWelcomeMessage to display on Welcome Modal for both type of learners

--- kolibri/plugins/device/assets/src/views/WelcomeModal.vue
+++ kolibri/plugins/device/assets/src/views/WelcomeModal.vue
@@ -27,7 +27,6 @@
   import { kolibriOnboardingGuideStrings } from 'kolibri/uiText/kolibriOnboardingGuideStrings';
   import useUser from 'kolibri/composables/useUser';
   import useFacilities from 'kolibri-common/composables/useFacilities';
-  import useLearnerResources from '../../../../../../kolibri/plugins/learn/assets/src/composables/useLearnerResources';
 
   export default {
     name: 'WelcomeModal',
